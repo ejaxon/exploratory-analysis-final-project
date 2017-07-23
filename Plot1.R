95ade1238a4f8aba206c76f9ee55eaaa55998fa1
ejaxon <eric@deepweave.com>
2017-07-23 08:48:30 -0400

    Add plot 2, change scaling for plot 1 back to Ktons

--- Plot1.R
+++ Plot1.R
@@ -3,10 +3,9 @@
 #    years 1999, 2002, 2005, and 2008.
 
 NEI <- readRDS("./summarySCC_PM25.rds")
-SCC <- readRDS("./Source_Classification_Code.rds")
-total <- tapply(NEI$Emissions, NEI$year, sum)/1000000 # Scale to Megatons
+total <- tapply(NEI$Emissions, NEI$year, sum)/1000 # Scale to Kilotons
 png(filename="./Plot1.png")
-barplot(total, main="Total U.S. PM2.5 Emissions (Megatons)", xlab="Year", ylab="Total PM2.5 Emissions (Megatons)", ylim=c(0,8))
+barplot(total, main="Total U.S. PM2.5 Emissions (Kilotons)", xlab="Year", ylab="Total PM2.5 Emissions (Kilotons)", ylim=c(0,8000))
 dev.off()
 
 
---
+++ Plot2.R
@@ -0,0 +1,11 @@
+# 2. Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (𝚏𝚒𝚙𝚜 == "𝟸𝟺𝟻𝟷𝟶")
+#    from 1999 to 2008? Use the base plotting system to make a plot answering this question.
+
+NEI <- readRDS("./summarySCC_PM25.rds")
+
+bc <- subset(NEI, fips==24510)
+total <- tapply(bc$Emissions, bc$year, sum)/1000 # Scale to Kilotons
+png(filename="./Plot2.png")
+barplot(total, main="Total Baltimore City PM2.5 Emissions (Kilotons)", xlab="Year", ylab="Total PM2.5 Emissions (Kilotons)", ylim=c(0,4))
+dev.off()
+
