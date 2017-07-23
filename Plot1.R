1620a99e37066273f13dc8b7849a0f522d5998e8
ejaxon <eric@deepweave.com>
2017-07-23 08:16:00 -0400

    Plot 1 code/png

---
+++ Plot1.R
@@ -0,0 +1,13 @@
+# 1. Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base
+#    plotting system, make a plot showing the total PM2.5 emission from all sources for each of the
+#    years 1999, 2002, 2005, and 2008.
+
+NEI <- readRDS("./summarySCC_PM25.rds")
+SCC <- readRDS("./Source_Classification_Code.rds")
+total <- tapply(NEI$Emissions, NEI$year, sum)/1000 # Scale to Kilotons
+png(filename="./Plot1.png")
+barplot(total, main="Total U.S. PM2.5 Emissions (Kilotons)", xlab="Year", ylab="Total PM2.5 Emissions (Kilotons)")
+dev.off()
+
+
+
