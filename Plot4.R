7245c963ab9fbb395b5ceb1a338420adf0eb3879
ejaxon <eric@deepweave.com>
2017-07-23 15:08:21 -0400

    Working on plot 4

--- Plot4.R
+++ Plot4.R
@@ -4,6 +4,11 @@
 NEI <- readRDS("./summarySCC_PM25.rds")
 SCC <- readRDS("./Source_Classification_Code.rds")
 
+scc.coalcomb<- subset(SCC, grepl(".*[Cc]oal.*", EI.Sector))
+scc.coalcomb$SCC <- factor(scc.coalcomb$SCC) # Recompute factor levels
+scc.coalcomb$EI.Sector <- factor(scc.coalcomb$EI.Sector) # Recompute factor levels
+
 png(filename="./Plot4.png")
 
 dev.off()
+
